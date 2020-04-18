12764032373ff991ec18329a85b6597b8ea3356d
Dixaba <dixaba@yandex.ru>
2020-04-18 19:32:54 +0300

    Added base Qt project with empty chart

--- TokenTracker/mainwindow.cpp
+++ TokenTracker/mainwindow.cpp
@@ -1,15 +1,52 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDateTime>
+#include <QTimer>
+
+int counter = 0;
+
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+  : QMainWindow(parent)
+  , ui(new Ui::MainWindow), s("DixabaCorp", "TokenTracker")
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
+  connect(this, &MainWindow::read,
+          this, &MainWindow::doneRead);
+  chart = ui->chart->chart();
+  tokens = new QLineSeries();
+  tokens->setName("Tokens");
+  chart->addSeries(dynamic_cast<QAbstractSeries *>(tokens));
+  xaxis = new QDateTimeAxis();
+  xaxis->setTickCount(5);
+  xaxis->setFormat("MM/dd hh:mm");
+  chart->addAxis(dynamic_cast<QAbstractAxis *>(xaxis), Qt::AlignBottom);
+  dynamic_cast<QAbstractSeries *>(tokens)->attachAxis(
+    dynamic_cast<QAbstractAxis *>(xaxis));
+  yaxis = new QValueAxis();
+  yaxis->setLabelFormat("%d");
+  chart->addAxis(dynamic_cast<QAbstractAxis *>(yaxis), Qt::AlignLeft);
+  dynamic_cast<QAbstractSeries *>(tokens)->attachAxis(
+    dynamic_cast<QAbstractAxis *>(yaxis));
+  QTimer::singleShot(10, this, &MainWindow::checkRead);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
 }
 
+void MainWindow::checkRead()
+{
+  if (s.status() != QSettings::NoError)
+    {
+      QTimer::singleShot(10, this, &MainWindow::checkRead);
+      counter++;
+    }
+  else
+    {
+      emit read();
+    }
+}
+
+void MainWindow::doneRead() {}
--- TokenTracker/mainwindow.h
+++ TokenTracker/mainwindow.h
@@ -2,20 +2,36 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSettings>
+#include <QtCharts>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+using namespace QtCharts;
+
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
-public:
+  public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private:
+  signals:
+    void read();
+
+  private slots:
+    void checkRead();
+    void doneRead();
+
+  private:
     Ui::MainWindow *ui;
+    QSettings s;
+
+    QChart *chart;
+    QLineSeries *tokens;
+    QDateTimeAxis *xaxis;
+    QValueAxis *yaxis;
 };
 #endif // MAINWINDOW_H
